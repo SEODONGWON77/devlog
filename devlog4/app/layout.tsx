dde4d52c846162317e0416a9f3936bfba1159c34
spicymaggiesoup <samantha8009@gmail.com>
2023-02-12 18:00:45 +0900

    setting: recoil

--- devlog4/app/layout.tsx
+++ devlog4/app/layout.tsx
@@ -2,6 +2,7 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SessionProvider } from "next-auth/react";
+import { RecoilRoot } from "recoil";
 import "../styles/globals.css";
 import Header from "./components/Header";
 import { getClient } from "./queryClient";
@@ -12,16 +13,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <QueryClientProvider client={getClient()}>
-      <html>
-        <head />
-        <SessionProvider>
-          <body className="border-4 border-red-400 p-2 w-full">
-            <Header />
-            {children}
-          </body>
-        </SessionProvider>
-      </html>
-    </QueryClientProvider>
+    <RecoilRoot>
+      <QueryClientProvider client={getClient()}>
+        <html>
+          <head />
+          <SessionProvider>
+            <body className="border-4 border-red-400 p-2 w-full">
+              <Header />
+              {children}
+            </body>
+          </SessionProvider>
+        </html>
+      </QueryClientProvider>
+    </RecoilRoot>
   );
 }

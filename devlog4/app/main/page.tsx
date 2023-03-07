26fc18e045231efae3ad4a4b5b8bcf9b2b9056c0
spicymaggiesoup <samantha8009@gmail.com>
2023-03-08 00:57:17 +0900

    1) 메인 리스트 영역 너비키움    2) 리스트 스타일 수정 중.. (충돌은 안날거에요)

--- devlog4/app/main/page.tsx
+++ devlog4/app/main/page.tsx
@@ -9,13 +9,13 @@ const Main = async ({}: Props) => {
     <div className="w-full">
       <div className="w-full h-[280px] bg-slate-400">캐러셀 컴포넌트</div>
       <div className="w-full flex">
-        <div className="w-[17.5%] bg-lime-100">광고 컴포넌트</div>
-        <div className="w-[65%]"> 
+        <div className="w-[15%] bg-lime-100">광고 컴포넌트</div>
+        <div className="w-[70%]"> 
           <div className="w-full h-20 bg-red-300">카테고리컴포넌트</div>
           {/* <TempPostList id={"id"}/> */}
           <List />
         </div>
-        <div className="w-[17.5%] bg-lime-100">광고 컴포넌트</div>
+        <div className="w-[15%] bg-lime-100">광고 컴포넌트</div>
       </div>
     </div>
   );

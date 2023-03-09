3cf40ecffc37929d6b32e6323741d43274ad215a
Dongwon <seo77j@gmail.com>
2023-03-09 18:53:10 +0900

    feature/index 추가

--- devlog4/models/post.ts
+++ devlog4/models/post.ts
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const postSchema = new mongoose.Schema({
   name: String,
   htmlStr: String,
+  index: Number,
 });
 
 export default mongoose.models.Post || mongoose.model("Post", postSchema);

a6f539aeef34b0cbc1f851c1a04a183c817c18fb
Dongwon <seo77j@gmail.com>
2023-05-14 18:47:47 +0900

    feature/connect server mongodb

--- devlog4/config/dbConnect.ts
+++ devlog4/config/dbConnect.ts
@@ -1,10 +1,8 @@
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
 
 const dbConnect = () => {
   if (mongoose.connection.readyState >= 1) return;
-
   mongoose.connect(process.env.MONGODB_URI!);
-  // mongoose.connect('mongodb+srv://admin:001@cluster0.szzbh.mongodb.net/test?retryWrites=true&w=majority');
 };
 
 export default dbConnect;

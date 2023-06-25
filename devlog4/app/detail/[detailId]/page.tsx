f91317fa9a664caccd010e1ae13ad19d9ec9d6f8
spicymaggiesoup <samantha8009@gmail.com>
2023-06-25 20:24:24 +0900

    detail merge

--- devlog4/app/detail/[detailId]/page.tsx
+++ devlog4/app/detail/[detailId]/page.tsx
@@ -7,10 +7,14 @@ import { userEmailState, userNameState } from "../../recoil/state";
 
 import { createAllRestFetchByDevlog } from "utils/api/fetch/devlogApiRestFetch";
 import { useQuery } from "@tanstack/react-query";
+
 import HashtagList from "../components/hashtag-list";
+
 import { detailResultService } from "app/service/detail";
 import { validateGetSearchResult } from "app/service/detail/utils/validate";
 import Toc from "../components/toc/Toc";
+
+import BookmarkButton from "../components/bookmark-button";
 
 const allFetch = createAllRestFetchByDevlog("post");
 
@@ -99,40 +103,14 @@ const DetailId = ({ params: { detailId } }: Props) => {
             </div>
           </div>
           <div className="flex justify-end">
-            <div className="inline-flex py-2">
-              <div>
-                <div className="pointer-events-auto relative inline-flex rounded-md bg-white text-[0.8125rem] font-medium leading-5 text-slate-700 shadow-sm ring-1 ring-slate-700/10 hover:bg-slate-50 hover:text-slate-900">
-                  <div className="flex px-3 py-2">
-                    <svg className="mr-2.5 h-5 w-5 flex-none fill-slate-400">
-                      <path d="M5 4a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v14l-5-2.5L5 18V4Z"></path>
-                    </svg>
-                    Bookmark
-                  </div>
-                  <div className="border-l border-slate-400/20 px-2.5 py-2">
-                    12k
-                  </div>
-                </div>
-              </div>
-              <div>
-                <button
-                  type="button"
-                  className="adn ajm aql bac bbi bin bot bou bow bpf"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    aria-hidden="true"
-                    className="nu rw"
-                  >
-                    <path d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z"></path>
-                  </svg>
-                </button>
-              </div>
+            <div className="inline-flex py-2">              
+              <BookmarkButton count={0}/>
             </div>
           </div>
-
-          <div className="w-full h-[100vh] bg-slate-400">
+          <div className="w-full">
+            <img src={details.previewImageUrl}></img>
+          </div>
+          <div className="w-full bg-slate-400">
             <div
               // className="bg-white border-slate-100 border-b rounded-t-xl"
               dangerouslySetInnerHTML={{
@@ -140,7 +118,6 @@ const DetailId = ({ params: { detailId } }: Props) => {
               }}
             />
           </div>
-          <div className="w-full"></div>
         </div>
       )
     );

1f01f64fae31921ec087b5daf548555b963fac52
Dongwon <seo77j@gmail.com>
2023-07-05 19:55:11 +0900

    feature search result list view

--- devlog4/app/main/List/components/Search.tsx
+++ devlog4/app/main/List/components/Search.tsx
@@ -4,32 +4,36 @@ import axios from "axios";
 import React, { useState } from "react";
 import { searchDropDownList } from "../constants";
 import DropDown from "./DropDown";
+import SearchInput from "./search-input";
 import { useSearch } from "./search-input/hooks/useSearch";
-const Search = () => {
-  const {
-    searchOriginalWord,
-    searchWord,
-    changeSearchWord,
-    searchBarKeyUp,
-    enteringSearchWord,
-    isSearch,
-  } = useSearch();
+import Image from "next/image";
 
-  const [searchString, setSearchString] = useState<string>("");
+interface SearchProps {
+  searchWord: string;
+  changeSearchWord: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  searchBarKeyUp: (e: React.KeyboardEvent<Element>) => void;
+  searchResult: any;
+}
 
-  const props = {
-    searchString,
-    searchWord,
-    changeSearchWord,
-    searchBarKeyUp,
-    enteringSearchWord,
-    searchDropDownList,
-    isSearch,
-  };
+const Search = ({
+  searchWord,
+  changeSearchWord,
+  searchBarKeyUp,
+  searchResult,
+}: SearchProps) => {
+
   return (
     <div className="flex align-middle justify-center mt-8">
-      <div>
-        <DropDown {...props} />
+      <div className="w-[50%] h-[3rem] p-[2px] flex border border-solid border-lightGray-20 rounded-md">
+        <div className="w-[30px] mt-3 ml-3">
+          <Image src={"/search.png"} width={20} height={20} alt="" />
+        </div>
+        <SearchInput
+          searchWord={searchWord}
+          changeSearchWord={changeSearchWord}
+          onKeyUp={searchBarKeyUp}
+          placeholder={`검색어를 입력해주세요.`}
+        />
       </div>
     </div>
   );

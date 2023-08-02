72eeaea05aae264bb68235eea085298d07a029c5
cowkite lee <35284787+spicymaggiesoup@users.noreply.github.com>
2023-08-02 20:01:42 +0900

    feature/likedcounter-models

--- devlog4/pages/api/post.ts
+++ devlog4/pages/api/post.ts
@@ -24,6 +24,7 @@ export default async function handler(
       index: seq === null ? 0 : seq,
       tagList: tagList,
       previewImageUrl: previewImageUrl,
+      likedCounter: 0,
     });
     res.status(201).json({ post });
   }
